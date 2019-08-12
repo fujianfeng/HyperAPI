5878fbb9b3482ae19dcc63125f1a1e250e7c3b18
Machmachi <badr.machmachi@bearingpoint.com>
2019-08-12 14:45:39 +0200

    add new routes to anaplan feature

--- HyperAPI/hdp_api/routes/anaplan.py
+++ HyperAPI/hdp_api/routes/anaplan.py
@@ -25,7 +25,22 @@ class Anaplan(Resource):
             'workspace_id': Route.VALIDATOR_OBJECTID,
             'model_id': Route.VALIDATOR_OBJECTID
         }
-        
+    class _getImports(Route):
+        name = "getImports"
+        httpMethod = Route.GET
+        available_since = '4.2.8'
+        path = "/anaplan/workspaces/{workspace_id}/model/{model_id}/imports/{dataset_Id}"
+        _path_keys = {
+            'workspace_id': Route.VALIDATOR_OBJECTID,
+            'model_id': Route.VALIDATOR_OBJECTID,
+            'dataset_Id': Route.VALIDATOR_OBJECTID
+        }
+    class _sendPayload(Route):
+        name = "sendPayload",
+        available_since = '4.2.8'
+        httpMethod = Route.POST
+        path = "/anaplan/send" 
+
     class _anaplanLogin(Route):
         name = "anaplanLogin"
         httpMethod = Route.POST
